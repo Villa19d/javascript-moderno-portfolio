b891bee960406592d495f8d27444abf6046b549c
Rodrigo <luisrodrigo1005@gmail.com>
2026-04-28 12:02:44 -0600

    fix carpetas nombre

--- 57-Administrador-de Citas-Typescript/56-admin-pacientes-ts/src/types.ts
+++
@@ -1,9 +0,0 @@
-export interface Cita {
-    [key : string] : string; //index signature 
-    id: string;
-    paciente: string;
-    propietario: string;
-    email: string;
-    fecha: string;
-    sintomas: string;
-}
