d8ea891c74d05734a1a1e600f1b545f565f6cbe6
Rodrigo <luisrodrigo1005@gmail.com>
2026-04-28 09:36:25 -0600

    Portafolio JS: Selección de proyectos principales

---
+++ 57-Administrador-de Citas-Typescript/56-admin-pacientes-ts/src/types.ts
@@ -0,0 +1,9 @@
+export interface Cita {
+    [key : string] : string; //index signature 
+    id: string;
+    paciente: string;
+    propietario: string;
+    email: string;
+    fecha: string;
+    sintomas: string;
+}
