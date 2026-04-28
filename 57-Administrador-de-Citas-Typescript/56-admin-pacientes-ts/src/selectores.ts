23aafd781e9038084a699252655dd2f97a7517c3
Rodrigo <luisrodrigo1005@gmail.com>
2026-04-28 12:03:34 -0600

    fix carpetas nombre2

---
+++ 57-Administrador-de-Citas-Typescript/56-admin-pacientes-ts/src/selectores.ts
@@ -0,0 +1,10 @@
+// Selectores
+export const pacienteInput = document.querySelector<HTMLInputElement>('#paciente') as HTMLInputElement
+export const propietarioInput = document.querySelector<HTMLInputElement>('#propietario') as HTMLInputElement
+export const emailInput = document.querySelector<HTMLInputElement>('#email') as HTMLInputElement
+export const fechaInput = document.querySelector<HTMLInputElement>('#fecha') as HTMLInputElement
+export const sintomasInput = document.querySelector<HTMLInputElement>('#sintomas') as HTMLInputElement
+
+export const formulario = document.querySelector<HTMLFormElement>('#formulario-cita') as HTMLFormElement
+export const formularioInput = document.querySelector<HTMLInputElement>('#formulario-cita input[type="submit"]') as HTMLInputElement
+export const contenedorCitas = document.querySelector<HTMLDivElement>('#citas') as HTMLDivElement
---
+++ 57-Administrador-de-Citas-Typescript/56-admin-pacientes-ts/vite.config.ts
@@ -0,0 +1,10 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+  base: './'
+})   
