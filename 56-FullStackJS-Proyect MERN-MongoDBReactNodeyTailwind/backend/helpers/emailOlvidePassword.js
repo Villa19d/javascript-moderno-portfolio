bbb6b254693d0c2af77901bfbd6c3f86742a28d7
Rodrigo <luisrodrigo1005@gmail.com>
2026-04-29 23:26:09 -0600

    email configurado

--- 56-FullStackJS-Proyect MERN-MongoDBReactNodeyTailwind/backend/helpers/emailOlvidePassword.js
+++ 56-FullStackJS-Proyect MERN-MongoDBReactNodeyTailwind/backend/helpers/emailOlvidePassword.js
@@ -3,6 +3,7 @@ const emailOlvidePassword = async (datos) => {
     const transport = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
+        secure: process.env.EMAIL_PORT === "465",
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
