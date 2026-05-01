a15898478d6e93eccda1e4b1384e8a4bcbfe2b6b
Rodrigo <luisrodrigo1005@gmail.com>
2026-05-01 14:40:57 -0600

    cambios para variables de entorno en producción

--- 56-FullStackJS-Proyect MERN-MongoDBReactNodeyTailwind/backend/index.js
+++ 56-FullStackJS-Proyect MERN-MongoDBReactNodeyTailwind/backend/index.js
@@ -5,18 +5,21 @@ import veterinarioRoutes from "./routes/veterinarioRoutes.js";
 import pacienteRoutes from "./routes/pacienteRoutes.js";
 import cors from "cors";
 
-console.log("ENV TEST:", process.env.MONGO_URI);
-
+// 1. Configurar dotenv ANTES que cualquier otra cosa
 dotenv.config();
 
 const app = express();
+
+// 2. Conectar a la DB
 connectDB();
 
-const dominiosPermitidos = [process.env.FRONTEND_URL]; // Aquí se define un array con los dominios permitidos para realizar solicitudes a la API. En este caso, se obtiene el valor del dominio permitido desde una variable de entorno llamada FRONTEND_URL.
+const dominiosPermitidos = [process.env.FRONTEND_URL];
 
 const corsOptions = {
-    origin: function (origin, callback) { // La función origin se utiliza para verificar si el origen de la solicitud está incluido en el array de dominios permitidos. Si el origen está permitido, se llama al callback con null y true para permitir la solicitud. Si el origen no está permitido, se llama al callback con un error indicando que el acceso a la API no está permitido desde ese dominio.
-        if (dominiosPermitidos.indexOf(origin) !== -1) {
+    origin: function (origin, callback) {
+        // Permitir peticiones sin origen (como Postman o Server-to-Server pings) 
+        // O si el origen está en nuestra lista blanca
+        if (!origin || dominiosPermitidos.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
             callback(new Error("Acceso a la API no permitido desde ese dominio por Cors"), false);
@@ -25,14 +28,13 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-// Middleware para parsear JSON
 app.use(express.json());
+
+// Tus rutas
 app.use("/api/veterinarios", veterinarioRoutes);
 app.use("/api/pacientes", pacienteRoutes);
 
 const PORT = process.env.PORT || 4000;
-
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
